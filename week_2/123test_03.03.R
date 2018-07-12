91e0bd9c525163a0413e7008e4034e331c5aa527
1221cc <40812690+1221cc@users.noreply.github.com>
2018-07-12 16:46:03 +0800

    存

--- week_2/123test_03.03.R
+++ week_2/123test_03.03.R
@@ -1,12 +1,12 @@
 source('pttTestFunction.R')
-id = c(1:3)
-URL = paste0("https://www.ptt.cc/bbs/NBA/search?page=", id, "&q=kobe")
+id = c(6128:6125)
+URL = paste0("https://www.ptt.cc/bbs/NBA/index", id, ".html")
 filename = paste0(id, ".txt")
 pttTestFunction(URL[1], filename[1])
-mapply(pttTestFunction, 
-       URL = URL, filename = filename)
+#mapply(pttTestFunction, 
+      # URL = URL, filename = filename)
 
 
-#網頁 https://www.ptt.cc/bbs/NBA/search?page=1&q=kobe 
+#網頁 https://www.ptt.cc/bbs/NBA/index6128.html
 
 
